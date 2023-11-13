39cafb05d83f688b8d6cedf2cbb15b2d643a8a68
nara04040 <nara04040@naver.com>
2023-11-14 02:56:17 +0900

    feat: DragStart, DragEnd, DragOver

--- [skill_archive]kanban/src/components/ColumnContainer.tsx
+++ [skill_archive]kanban/src/components/ColumnContainer.tsx
@@ -1,5 +1,7 @@
 import { Column, Id } from "../types";
 import TrashIcon from "../icons/TrashIcon";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   column: Column;
@@ -7,8 +9,38 @@ interface Props {
 }
 
 const ColumnContainer = ({ column, deleteColumn }: Props) => {
+  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+    id: column.id,
+    data: { type: "Column", column },
+  });
+
+  const style = { transition, transform: CSS.Transform.toString(transform) };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="
+      bg-columnBackgroundColor
+      opacity-40
+      border-2
+      border-pink-500
+      w-[350px]
+      h-[500px]
+      max-h-[500px]
+      rounded-md
+      flex
+      flex-col
+      "
+      >
+        hi
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
       <div className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between">
         <div className="flex gap-2">
           <div className=" flex justify-center items-center bg-columnBackgroundColor px-2 py-1 text-sm rounded-full">0</div>
--- [skill_archive]kanban/src/components/KanbanBoard.tsx
+++ [skill_archive]kanban/src/components/KanbanBoard.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Column, Id, Task } from "../types";
 import PlusIcon from "../icons/PlusIcon";
 import ColumnContainer from "./ColumnContainer";
+import { DndContext, DragEndEvent, DragOverEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 
 const defaultCols: Column[] = [
   {
@@ -26,6 +29,14 @@ const defaultTasks: Task[] = [
 
 const KanbanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+  const sensor = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    })
+  );
 
   const createNewColumn = () => {
     const columnToAdd: Column = {
@@ -45,25 +56,55 @@ const KanbanBoard = () => {
     setColumns(filteredColumns);
   };
 
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+
+  const onDragStart = (e: DragStartEvent) => {
+    console.log("drag start", e);
+
+    if (e.active.data.current?.type === "Column") {
+      setActiveColumn(e.active.data.current.column);
+      return;
+    }
+  };
+
+  const onDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+    console.log("drag end", active, over);
+  };
+  const onDragOver = (e: DragOverEvent) => {
+    const { active, over } = e;
+    const activeId = active.id;
+    const overId = over?.id;
+
+    if (!over) return;
+
+    if (activeId === overId) return;
+  };
   console.log(columns);
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center justify-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
-        <div className="flex gap-2">
-          {columns.map((col) => (
-            <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
-          ))}
+      <DndContext sensors={sensor} onDragStart={onDragStart} onDragEnd={onDragEnd} onDragOver={onDragOver}>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
+              ))}
+            </SortableContext>
 
-          <button
-            onClick={() => createNewColumn()}
-            className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
-          >
-            <PlusIcon />
-            Add Column
-          </button>
+            <button
+              onClick={() => createNewColumn()}
+              className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+            >
+              <PlusIcon />
+              Add Column
+            </button>
+          </div>
+
+          {createPortal(<DragOverlay>{activeColumn && <ColumnContainer column={activeColumn} deleteColumn={deleteColumn} />}</DragOverlay>, document.body)}
         </div>
-      </div>
+      </DndContext>
     </div>
   );
 };
