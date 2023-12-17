abbd13938aa0a68beffc304b7488bd43e0024a52
nara04040 <nara04040@naver.com>
2023-12-18 03:35:35 +0900

    study node create event

--- [skill_archive]light_Node_Server/create_new_event.js
+++ [skill_archive]light_Node_Server/create_new_event.js
@@ -2,8 +2,9 @@ var EventEmiiter = require("events");
 
 var custom_event = new EventEmiiter();
 
+// 이벤트 등록
 custom_event.on("call", function () {
   console.log("이벤트 콜");
 });
 
-custom_event.emit("call");
+custom_event.emit("call"); // 이벤트 강제 발생
